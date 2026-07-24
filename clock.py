470a0b1e22cff4ee63461a4df60afe19278e47d8
Md Moinuddin <191840000+Md-Moinuddin01@users.noreply.github.com>
2026-07-25 01:27:20 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,1 +1,14 @@
+from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
+from pathlib import Path
+import os
 
+
+project_folder = Path(__file__).parent
+os.chdir(project_folder)
+
+try:
+    server.serve_forever()
+except KeyboardInterrupt:
+    print("\nClock stopped.")
+finally:
+    server.server_close()
