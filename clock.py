8421e1b096368ce45c36af728ace82557d9d0678
Md Moinuddin <191840000+Md-Moinuddin01@users.noreply.github.com>
2026-09-04 14:28:51 +0530

    Correct server method call and import sys
    Fix typo in server method call and add sys import.

--- clock.py
+++ clock.py
@@ -1,6 +1,7 @@
 from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
 from pathlib import Path
 import os
+import sys
 
 
 project_folder = Path(__file__).parent
@@ -8,6 +9,7 @@ os.chdir(project_folder)
 
 try:
     server.serve_forever()
+    server.server_forever() 12
 except KeyboardInterrupt:
     print("\nClock stopped.")
 finally:
